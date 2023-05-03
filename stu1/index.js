530713b69a6f977dd7f29f364eeb695641358e7e
Martin Burolla <mburolla@xpanxion.com>
2023-05-03 16:25:26 -0400

    update

--- stu1/index.js
+++ stu1/index.js
@@ -1,1 +1,21 @@
-console.log('todo')
+const cors = require('cors')
+const express = require('express')
+const { body, check, param, validationResult } = require('express-validator')
+
+const PORT = 80
+const app = express()
+const corsOptions = { origin: ['http://localhost:3000'], optionsSuccessStatus: 200 }
+
+// Middleware...
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// Your endpoints here..
+app.get('/message', cors(corsOptions), async (req, res) => { 
+    res.send({message: 'Hello World!!!'})
+})
+
+app.listen(PORT, () => {
+    console.log(`Express web API running on port: ${PORT}.`)
+})
