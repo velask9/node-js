f4355f1c255fcb9e24418bbac07ad5655a3120d4
Karina Velasquez <kvelasquez@xpanxion.com>
2023-06-07 15:44:24 -0400

    EX3

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express')
-const { body, check, param, validationResult } = require('express-validator')
+const { body, check, param, validationResult, Result } = require('express-validator')
+const pool=require('./PromisePool').promisePool
 
 const PORT = 80
 const app = express()
@@ -12,8 +13,31 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // Your endpoints here..
+//ex 1 endpoint
+app.get('/message', cors(corsOptions),async(req,res)=>{
+    res.send({message:'Hello World!!!'})
+})
+
+//ex2 endpoint
+app.get('/car/:id', cors(corsOptions), async(req, res)=>{
+    let car_id = req.params['id']
+    const [result] = await pool.query('Select * FROM car where car_id= ?', [car_id])
+    //Read request body. 
+    // const body = result [0]
+    res.send(result)
+    }) 
+
+//ex3 endpoint
+app.get('/cars', cors(corsOptions), async(req, res)=>{
+    let make = req.query['make']
+    const [result] = await pool.query('Select * FROM car where make= ?', [make])
+    res.send(result)
+    }) 
 
 
 app.listen(PORT, () => {
     console.log(`Express web API running on port: ${PORT}.`)
 })
+
+
+
