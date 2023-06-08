3a9876ce207537aa70cc9673d7a209df96206756
Karina Velasquez <kvelasquez@xpanxion.com>
2023-06-08 13:51:55 -0400

    Ex6

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const cors = require('cors')
 const express = require('express')
 const { body, check, param, validationResult, Result } = require('express-validator')
+const { result } = require('lodash')
 const pool=require('./PromisePool').promisePool
 
 const PORT = 80
@@ -11,6 +12,7 @@ const corsOptions = { origin: ['http://localhost:3000'], optionsSuccessStatus: 2
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 
 // Your endpoints here..
 //ex 1 endpoint
@@ -36,7 +38,9 @@ app.get('/cars', cors(corsOptions), async(req, res)=>{
 
 //ex4 endpoint
 app.post('/cars', cors(corsOptions), async(req, res) =>{
+    //Destructuring req.body
     const {model, make, color, price} = req.body;
+    //Query to insert car on the table
     const [result]= await pool.query('insert into car(model, make, color, price) values (?,?,?,?)',
     [model, make, color, price])
     
@@ -53,6 +57,17 @@ app.put('/cars', cors(corsOptions), async (req, res) =>{
     res.send(result) 
     
 })
+
+//ex6 endpoint
+app.delete('/cars/:id', cors(corsOptions), async (req, res) =>{
+   let car_id= req.params['id']
+    const [result] = await pool.execute("delete from car where car_id = ?",[car_id])
+    res.send(result)
+ 
+})
+
+//ex7 endpoint
+
 
 
 app.listen(PORT, () => {
